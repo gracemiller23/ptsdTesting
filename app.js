4f80c2b7ce81556da6da52a9a4d722c82e74d81a
gracemiller23 <gracemiller0323@gmail.com>
2018-05-16 23:33:12 -0500

    requirejs might work?

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+
+
 
 // //variables for text analysis functions
 var descriptionTextResultsArray =[];
